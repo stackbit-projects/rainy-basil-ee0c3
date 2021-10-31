75860cc381418af5f2c231010828fc16379b988c
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 18:47:08 +0000

    testimonials-section.js: changed

--- src/components/testimonials-section.js
+++ src/components/testimonials-section.js
@@ -17,7 +17,7 @@ const TestimonialsSection = (props) => {
         {testimonials.map((testimonial, index) => (
           <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
             <h2 className="text-sm text-gray-400" data-sb-field-path=".name">
-              {testimonial.}
+              {testimonial.body}
             </h2>
           </div>
         ))}

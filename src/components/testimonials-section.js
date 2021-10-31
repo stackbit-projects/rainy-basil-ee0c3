671e6ac6d873ea86964f9d216999b9e6a3e47d23
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 18:46:42 +0000

    testimonials-section.js: changed

--- src/components/testimonials-section.js
+++ src/components/testimonials-section.js
@@ -16,13 +16,6 @@ const TestimonialsSection = (props) => {
         data-sb-field-path=".logos">
         {logos.map((logo, index) => (
           <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
-            <img
-              className="mb-2"
-              height="60px"
-              width="60px"
-              src={logo.image}
-              data-sb-field-path=".image#@src"
-            />
             <h2 className="text-sm text-gray-400" data-sb-field-path=".name">
               {logo.name}
             </h2>

f78082e419ab26fe6a2f42d196fe8b8087400bc2
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 18:55:09 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -11,8 +11,8 @@ const TestimonialsSection = (props) => {
             </h1>
             <div
                 className="flex justify-center items-center"
-                data-sb-field-path=".logos">
-                {logos.map((logo, index) => (
+                data-sb-field-path=".testimonials">
+                {testimonials.map((logo, index) => (
                     <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
                         <img
                             className="mb-2"

a1bcb8cdcc5dd13e53fc7495d85a1744dd5ecd5b
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 19:00:53 +0000

    LogoSection.js: changed

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -14,7 +14,7 @@ const LogoSection = (props) => {
                 data-sb-field-path=".testimonials">
                 {testimonials.map((testimonial, index) => (
                     <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
-                        <h2 className="text-sm text-gray-400" data-sb-field-path=".name">
+                        <h2 className="text-sm text-gray-400" data-sb-field-path=".header">
                             {logo.name}
                         </h2>
                     </div>

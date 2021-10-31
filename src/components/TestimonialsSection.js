d6e895d758ec93fdbea84880566e1531e0feb552
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 18:55:58 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -4,22 +4,7 @@ const TestimonialsSection = (props) => {
         <div
             className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center"
             data-sb-field-path={annotationPrefix}>
-            <h1
-                className="text-3xl tracking-tight sm:text-4xl mb-2"
-                data-sb-field-path=".title">
-                {title}
-            </h1>
-            <div
-                className="flex justify-center items-center"
-                data-sb-field-path=".testimonials">
-                {testimonials.map((testimonial, index) => (
-                    <div className="p-6" key={index} data-sb-field-path={`.${index}`}>
-                        <h2 className="text-sm text-gray-400" data-sb-field-path=".title">
-                            {testimonials.title}
-                        </h2>
-                    </div>
-                ))}
-            </div>
+            
         </div>
     );
 };

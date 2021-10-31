3c8584908b53c351eb27b97de496c7faf849bfe2
Stackbit Code Editor <projects@stackbit.com>
2021-10-31 18:55:22 +0000

    TestimonialsSection.js: changed

--- src/components/TestimonialsSection.js
+++ src/components/TestimonialsSection.js
@@ -22,7 +22,7 @@ const TestimonialsSection = (props) => {
                             data-sb-field-path=".image#@src"
                         />
                         <h2 className="text-sm text-gray-400" data-sb-field-path=".title">
-                            {logo.name}
+                            {testimonials.name}
                         </h2>
                     </div>
                 ))}
